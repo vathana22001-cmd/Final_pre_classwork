4d7ce82f4010502d9c130276240ab299effc25bc
vathana22001-cmd <vathana22001@gmail.com>
2026-07-17 21:52:49 +0700

    Update code

--- src/product.cpp
+++ src/product.cpp
@@ -1,10 +1,1 @@
-#pragma once
-#include <string>
-
-struct Product {
-    int id;
-    std::string title;
-    std::string description;
-    double price;
-    int stock;
-};
+#include "product.hpp"
